630266c8089905de8fb72782c4409d7ac0005418
Carlos Alberto Graniel Córdova DEV <caco572@gmail.com>
2018-06-30 12:21:05 -0500

    Agregar archivos de proyecto.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Button NavigateButton = new Button
+            {
+                Text = "Navegar",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+
+            NavigateButton.Clicked += async delegate
+            {
+                await Navigation.PushAsync(new HelloCode());
+            };
+
+            Content = NavigateButton;
+		}
+	}
+}
