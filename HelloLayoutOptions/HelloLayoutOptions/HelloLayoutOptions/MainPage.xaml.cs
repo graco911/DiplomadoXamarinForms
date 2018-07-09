2f38c08c95d2bc9e4b69b5772c506a568db787c7
Carlos Alberto Graniel Córdova <caco572@gmail.com>
2018-07-08 21:34:59 -0500

    agregado ejercicio

---
+++ HelloLayoutOptions/HelloLayoutOptions/HelloLayoutOptions/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HelloLayoutOptions
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            //Content = new StackLayout
+            //{
+            //    Orientation = StackOrientation.Horizontal,
+            //    Children =
+            //    {
+            //        new Label
+            //        {
+            //            Text = "Start",
+            //            BackgroundColor = Color.DarkOrange,
+            //            VerticalOptions = LayoutOptions.Start
+            //        },
+            //        new Label
+            //        {
+            //            Text = "Center",
+            //            BackgroundColor = Color.DarkOrange,
+            //            VerticalOptions = LayoutOptions.Center
+            //        },
+            //        new Label
+            //        {
+            //            Text = "End",
+            //            BackgroundColor = Color.DarkOrange,
+            //            VerticalOptions = LayoutOptions.End
+            //        },
+            //        new Label
+            //        {
+            //            Text = "Fill",
+            //            BackgroundColor = Color.DarkOrange,
+            //            VerticalOptions = LayoutOptions.Fill
+            //        }
+            //    }
+            //};
+        }
+	}
+}
